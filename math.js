daa2e999e19af59c496319f092e10e1a96f9277c
lawrence1130 <samlawrencedadang@gmail.com>
2025-11-18 08:08:16 +0800

    arithmitic

---
+++ math.js
@@ -0,0 +1,53 @@
+//My code
+num1 = 2;
+num2 = 3;
+
+
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+function multiply(num1,num2){
+    return num1 * num2;
+}   
+function divide(num1,num2){
+    return num1 / num2;
+}
+
+console.log(add(num1,num2));
+console.log(subtract(num1,num2));
+console.log(multiply(num1,num2));
+console.log(divide(num1,num2));
+
+//Sir Cliff's code
+function addition(num1,num2){
+    addition = num1 + num2;
+    console.log(addition);
+}
+
+addition(5,10);
+
+function subtract(num1,num2){
+    subtract = num1 + num2;
+    console.log(subtract);
+}
+
+subtract(5,10);
+
+function multiply(num1,num2){
+    multiply = num1 * num2;
+    console.log(multiply);
+}  
+
+multiply(5,10);
+
+function divide(num1,num2){
+    divide = num1 / num2;
+    console.log(divide);
+}
+
+divide(10,5);
